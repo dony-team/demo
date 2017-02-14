1df80f77afa9697540958e80e793cf4b70eba2c4
Anytao <anytao@live.com>
2017-02-14 13:18:28 +0800

    create a new file
    创建一个新文件

---
+++ app.js
@@ -0,0 +1,20 @@
+const _initRun = function () {
+
+    _initLang();
+
+    $(document).ready(function () {
+        var pathname = window.location.pathname.replace('/zh-cn', '').replace('/zh-tw', '').replace('/en-us', '');
+        if (router[pathname]) {
+            router[pathname]();
+        }
+    });
+
+    //语言切换
+    $('#language-switch').change(function () {
+        var current = $(this).attr("current");
+        var value = $(this).val();
+        location.href = "/" + value + "/" + (current == 'index' ? '' : current);
+    });
+};
+
+_initRun();
